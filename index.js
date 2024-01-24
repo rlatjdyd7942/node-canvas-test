e55cc2ebc2d2e4b116678d8cdbfac2b5714090a9
rlatjdyd7942 <seongyongkim9@gmail.com>
2024-01-24 22:00:08 +1000

    html canvas image download

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-import { createCanvas, registerFont, Image } from "canvas";
-import * as fs from 'fs';
+import express from 'express';
+const app = express();
+const port = 3000;
 
-const canvas = createCanvas(1050, 200);
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-const ctx = canvas.getContext('2d')
-const imageUrl = './41e7407f-706a-478a-9836-1dc900f46c72 (2).jpeg'
-const image = new Image()
-image.onload = function() {
-    ctx.drawImage(image, 0, 0, 1050, 200)
-}
-image.src = imageUrl
-// const myFont = new FontFace(
-//   "Pangolin",
-//   "url(https://fonts.gstatic.com/s/pangolin/v6/cY9GfjGcW0FPpi-tWMfN79z4i6BH.woff2)"
-// )
-// await myFont.load()
-// document.fonts.add(myFont)
-registerFont("RubikBurned-Regular.ttf", { family: "RubikBurned" });
-ctx.fillStyle = "#EEEEEE";
-ctx.font = "30px RubikBurned";
-ctx.textBaseline = 'top'
-ctx.fillText("A chicken", 100, 100);
-
-const buffer = canvas.toBuffer("image/png");
-fs.writeFileSync("./image.png", buffer);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
